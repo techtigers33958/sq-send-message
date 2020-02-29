87df5487a76d01fa643a106e1d3100270604752a
techtigers33958 <53841748+techtigers33958@users.noreply.github.com>
2020-02-29 10:18:35 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+function release_ball () {
+    pins.servoWritePin(AnalogPin.P4, 45)
+    basic.pause(1000)
+    pins.servoWritePin(AnalogPin.P4, 100)
+}
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "START") {
+        reset()
+    } else if (receivedString == targetString) {
+        basic.showIcon(IconNames.Yes)
+        release_ball()
+    } else {
+        basic.showIcon(IconNames.No)
+        basic.showString("GOT:" + receivedString + "WANTED:" + targetString)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    reset()
+})
+function reset () {
+    targetString = "hello world"
+}
+input.onButtonPressed(Button.A, function () {
+    release_ball()
+})
+let targetString = ""
+radio.setGroup(30)
+pins.analogSetPeriod(AnalogPin.P4, 20000)
+pins.servoSetPulse(AnalogPin.P4, 2000)
+basic.showIcon(IconNames.Yes)
