5c0d32794b831ba91473cf76718fe8b329596ca3
techtigers33958 <53841748+techtigers33958@users.noreply.github.com>
2020-03-01 09:58:09 -0500

    Rearrage the blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+function reset () {
+    targetString = "hello world"
+    basic.showIcon(IconNames.Square)
+}
 function release_ball () {
     pins.servoWritePin(AnalogPin.P4, 45)
     basic.pause(1000)
@@ -17,10 +21,6 @@ radio.onReceivedString(function (receivedString) {
 input.onButtonPressed(Button.B, function () {
     reset()
 })
-function reset () {
-    targetString = "hello world"
-    basic.showIcon(IconNames.Square)
-}
 input.onButtonPressed(Button.A, function () {
     release_ball()
 })
