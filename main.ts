3232bf0be208ffc97359e559223f50c29fd89dd3
techtigers33958 <53841748+techtigers33958@users.noreply.github.com>
2020-03-07 10:13:35 -0500

    Comment code

--- main.ts
+++ main.ts
@@ -1,28 +1,61 @@
-function release_ball () {
-    pins.servoWritePin(AnalogPin.P4, 45)
-    basic.pause(1000)
-    pins.servoWritePin(AnalogPin.P4, 100)
-}
 radio.onReceivedString(function (receivedString) {
+    // Checks if receivedString = "START"
+    //
+    // -if yes, program resets
+    //
+    // -does this if challenge is left half done
+    //
+    //
+    // Else, checks if 
+    //
+    // receivedString = targetString
+    //
+    // -if yes, shows check icon and releases ball
+    //
+    // -if no, shows "x" icon and displays message: GOT:
+    // START, WANTED: HELLO WORLD.
     if (receivedString == "START") {
         reset()
     } else if (receivedString == targetString) {
         basic.showIcon(IconNames.Yes)
-        release_ball()
+        releaseBall()
     } else {
         basic.showIcon(IconNames.No)
         basic.showString("GOT:" + receivedString + "WANTED:" + targetString)
     }
 })
+// Drops the ball:
+//
+// -pushes ball 
+//
+// -waits 1 sec
+//
+// -moves motor back
+function releaseBall () {
+    pins.servoWritePin(AnalogPin.P4, 45)
+    basic.pause(1000)
+    pins.servoWritePin(AnalogPin.P4, 100)
+}
+// For manual control:
+//
+// -resets the program on demand
 input.onButtonPressed(Button.B, function () {
     reset()
 })
+// Resets the program:
+//
+// -sets targetString to "hello world"
+//
+// -sets icon to diamond
 function reset () {
-    targetString = "hello world"
+    targetString = "HELLO WORLD"
     basic.showIcon(IconNames.Diamond)
 }
+// For manual control:
+//
+// -releases the ball on demand if it jams in tube.
 input.onButtonPressed(Button.A, function () {
-    release_ball()
+    releaseBall()
 })
 let targetString = ""
 radio.setGroup(10)
